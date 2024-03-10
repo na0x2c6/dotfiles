72154a18115299beedeaf95f485f4629021e1344
UEDA Naoaki <nao.uedder@gmail.com>
2024-03-10 23:12:03 +0900

    Add powerlevel10k

--- run_once_before_app-install.sh
+++ run_once_before_app-install.sh
@@ -9,3 +9,7 @@ fi
 
 sudo dnf install -y copyq tmux wl-clipboard \
     adobe-source-han-code-jp-fonts adobe-source-han-sans-jp-fonts
+
+if [[ ! -e ~/.powerlevel10k ]] ; then
+    git clone --filter=blob:none https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
+fi
