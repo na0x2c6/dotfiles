bc1cfaa176c7cec29a856e342b267d4b523c0f2f
UEDA Naoaki <nao.uedder@gmail.com>
2024-03-10 23:08:43 +0900

    Add tmux configs

--- run_once_before_app-install.sh
+++ run_once_before_app-install.sh
@@ -7,4 +7,5 @@ if [[ $(uname -s) != "Linux" ]] ; then
     exit 0
 fi
 
-sudo dnf install -y copyq adobe-source-han-code-jp-fonts adobe-source-han-sans-jp-fonts
+sudo dnf install -y copyq tmux wl-clipboard \
+    adobe-source-han-code-jp-fonts adobe-source-han-sans-jp-fonts
