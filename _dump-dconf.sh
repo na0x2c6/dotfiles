0fcdba5e8cd4d43a9244d9801fd39c7a355b9942
UEDA Naoaki <nao.uedder@gmail.com>
2024-03-17 10:26:49 +0900

    Remove needless line

--- _dump-dconf.sh
+++ _dump-dconf.sh
@@ -5,7 +5,6 @@ set -Eeuvo pipefail
 pushd $(dirname $0)
 
 dconf dump /org/gnome/desktop/wm/keybindings/             > dconf-org-gnome-desktop-wm-keybindings.ini
-dconf dump /org/gnome/desktop/peripherals/touchpad/       > dconf-org-gnome-desktop-peripherals-touchpad.ini
 dconf dump /org/gnome/shell/keybindings/                  > dconf-org-gnome-shell-keybindings.ini
 dconf dump /org/gnome/settings-daemon/plugins/media-keys/ > dconf-org-gnome-settings-daemon-plugins-media-keys.ini
 dconf dump /org/gnome/desktop/interface/                  > dconf-org-gnome-dektop-interface.ini
