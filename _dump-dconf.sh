b936fa7b3794714fbacddf2485706b5ab54803a5
UEDA Naoaki <nao.uedder@gmail.com>
2025-10-05 15:48:14 +0900

    Fix some keyboard shortcuts

--- _dump-dconf.sh
+++ _dump-dconf.sh
@@ -8,6 +8,7 @@ dconf dump /org/gnome/desktop/wm/keybindings/             > dconf-org-gnome-desk
 dconf dump /org/gnome/shell/keybindings/                  > dconf-org-gnome-shell-keybindings.ini
 dconf dump /org/gnome/settings-daemon/plugins/media-keys/ > dconf-org-gnome-settings-daemon-plugins-media-keys.ini
 dconf dump /org/gnome/desktop/interface/                  > dconf-org-gnome-dektop-interface.ini
-dconf dump /org/gnome/terminal/legacy/profiles:/          > dconf-org-gnome-terminal-legacy-profiles.ini
+dconf dump /org/gnome/terminal/legacy/                    > dconf-org-gnome-terminal-legacy.ini
+dconf dump /org/gnome/mutter/                             > dconf-org-gnome-mutter.ini
 
 popd
